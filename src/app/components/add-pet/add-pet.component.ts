440b17174df59f871ae179844cc83766ba590641
Lazcano007 <Pedrolazcano@live.com>
2025-05-12 13:40:16 +0200

    Created add-pet component

---
+++ src/app/components/add-pet/add-pet.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { PetService } from '../../service/pet.service';
+
+@Component({
+  selector: 'app-add-pet',
+  imports: [RouterModule, CommonModule, FormsModule],
+  templateUrl: './add-pet.component.html',
+  styleUrl: './add-pet.component.scss'
+})
+export class AddPetComponent {
+  pet = {
+    owner: '',
+    name: '',
+    species: '',
+    breed: '',
+    age: '',
+  };
+
+  constructor (private petService: PetService, private router: Router) {}
+
+  addPet () {
+    const userId = localStorage.getItem('userId');
+    if(!userId) return;
+
+    this.pet.owner = userId;
+    const petData = { ...this.pet};
+
+    this.petService.createPet(petData).subscribe({
+      next: () => {
+      this.router.navigate(['/dashboard'])
+    },
+      error: (err: unknown) => {
+        if (err instanceof Error) {
+          console.log('Theres been an error creating a pet', err);
+        }
+      }
+    });
+  }
+}
